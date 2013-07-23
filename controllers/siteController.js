bb22b35cb0688cf25dd820cb91f1e3f5ea4d07c8
chunterg <chunterg@gmail.com>
2013-07-23 16:48:56 +0800

    增加用户注册登录 增加gbk支持

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,10 +1,13 @@
 var fs = require('fs');
-var path = require('path')
+var path = require('path');
+var iconv = require('../node_modules/iconv-lite');
 SiteController = function (app, mongoose, cfg) {
 
     //include
     app.get('*.html|*.htm', function(req, res, next) {
-        var content = fs.readFileSync(cfg.root + req.path, 'utf8');
+        var content = fs.readFileSync(cfg.root + req.path);
+        content = iconv.decode(content, cfg.encode);
+        res.charset = cfg.encode;
         res.end(content)
     });
     // app.get(cfg.siteDirectory+'404/?', function(req, res, next) {
